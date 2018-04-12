3410033e993a9a14a5960c1f3cb3c23135ceae59
punarvasu510 <punarvasu510@gmail.com>
2018-04-12 13:23:10 +0530

    datetime format changes

--- Flask_trial/flask-env/app.py
+++ Flask_trial/flask-env/app.py
@@ -1,13 +1,6 @@
 from flask import Flask,render_template,json, request,redirect,url_for, jsonify
-<<<<<<< HEAD
 from userQuery import UserQuery
 from datetime import datetime
-=======
-from datetime import datetime
-import logging
-
-from userQuery import UserQuery
->>>>>>> a7ae82dac1ff1a878105103088b25013db65a4c7
 
 app = Flask(__name__)
 
@@ -37,7 +30,6 @@ def intruderClipsFirst():
 
 @app.route('/getTotalCountNext')
 def getTotalCountNext():
-<<<<<<< HEAD
     t1 = str(request.values.get("t1"))
     t2 = str(request.values.get("t2"))
 
@@ -63,37 +55,6 @@ def getIntrudersNext():
 
     intruder_list  = UserQuery.get_intruders(t1,t2)
     return "The list of intruders in the range " + str(t1) + " and " + str(t2) + " is :"
-=======
-	print "inside"
-	
-	print request.values
-	
-	print "after"
-	t1 = request.values.get("t1")
-	t2 = request.values.get("t2")
-	
-	print "T1: %s, T2: %s" % (t1,  t2)
-	
-	try:
-		# Input format: YYYY-mm-DDTHH:MM
-		t1 = t1.replace('T', ' ').replace('Z', '')
-		t1 += ":00"
-		
-		t2 = t2.replace('T', ' ').replace('Z', '')
-		t2 += ":00"
-	
-		count = UserQuery.get_intruder_count(t1,t2)
-		return "The total count of intruders in range " + t1 + " and " + t2 + " is " + str(count)
-	except Exception as e:
-		logging.exception("Error while processing")
-
-@app.route('/getIntrudersNext')
-def getIntrudersNext():
-     t1 = request.values.get(t1)
-     t2 = request.values.get(t2)
-     intruder_list  = getIntruders(t1,t2)
-     return "The list of intruders in the range " + t1 + " and " + t2 + " is :"
->>>>>>> a7ae82dac1ff1a878105103088b25013db65a4c7
 
 if __name__ == '__main__':
    app.run()
