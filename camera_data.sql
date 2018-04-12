1ed525bd553f416675073c933c43a1351787c99c
Vineetha Nadimpalli <nvineetha@gmail.com>
2018-04-12 09:46:58 +0530

    Add files via upload

---
+++ camera_data.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select* from camera_data
+LIMIT 0, 1000
+
+-- Date: 2018-04-12 09:46
+*/
+INSERT INTO `camera_data` (`id`,`location`,`description`) VALUES (1,'Entrance','This camera monitors the entrance to main building');
+INSERT INTO `camera_data` (`id`,`location`,`description`) VALUES (2,'Office','This camera monitors the office area');
