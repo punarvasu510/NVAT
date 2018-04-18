834a252c6c94c55092c2009b027f70288f799636
punarvasu510 <punarvasu510@gmail.com>
2018-04-18 13:02:08 +0530

    UI changes

--- Flask_trial/flask-env/app.py
+++ Flask_trial/flask-env/app.py
@@ -7,31 +7,15 @@ import os, io
 app = Flask(__name__)
 
 @app.route('/')
-def test():
-   return "Novel Video Analytics and Tapestry"
-
-###### Home page
 @app.route('/home')
 def home():
-    return render_template('home.html')
-######
+   return render_template('home.html')
+   #return "Novel Video Analytics and Tapestry"
 
-##### Rendered on home page as buttons
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 @app.route('/getTotalCount')
-def getTotalCountFirst():
-    return render_template('getTotalCount.html')
-
-@app.route('/getIntruders')
-def getIntrudersFirst():
-    return render_template('getIntruders.html')
-
-@app.route('/intruderClips')
-def intruderClipsFirst():
-    return render_template('intruderClips.html')
-######
-
-@app.route('/getTotalCountNext')
-def getTotalCountNext():
+def getTotalCount():
     t1 = str(request.values.get("t1"))
     t2 = str(request.values.get("t2"))
 
@@ -42,10 +26,12 @@ def getTotalCountNext():
     t2 = datetime.strptime(t2, "%Y-%m-%d %I:%M:%S")
 
     count = UserQuery.get_intruder_count(t1,t2)
-    return "The total count of intruders in range " + str(t1) + " and " + str(t2) + " is " + str(count)
+    return render_template('TotalCount.html', t1=str(t1), t2=str(t2), count=count)
 
-@app.route('/getIntrudersNext')
-def getIntrudersNext():
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/getIntruderProfiles')
+def getIntruderProfiles():
     t1 = str(request.values.get("t1"))
     t2 = str(request.values.get("t2"))
 
@@ -57,10 +43,10 @@ def getIntrudersNext():
 
     intruder_id_list = UserQuery.get_intruders(t1,t2)
 
-    return render_template('getIntruders.html', list=True, idlist=intruder_id_list, t1=t1, t2=t2)
+    return render_template('IntruderProfiles.html', idlist=intruder_id_list, t1=t1, t2=t2)
 
-@app.route('/getVideoClips')
-def getVideoClipsFirst():
+@app.route('/getTapestry')
+def getTapestry():
     t1 = str(request.values.get("t1"))
     t2 = str(request.values.get("t2"))
     intruderid = request.values.get("intid")
@@ -69,10 +55,12 @@ def getVideoClipsFirst():
 
     #print str(video_clips_list)
 
-    return render_template('getVideoClips.html', t1=t1, t2=t2, intid=intruderid, video_list = video_clips_list)
+    return render_template('Tapestry.html', t1=t1, t2=t2, intid=intruderid, video_list = video_clips_list)
 
-@app.route('/getVideoClipsNext')
-def getVideoClipsNext():
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/getVideoClips')
+def getVideoClips():
 	print "hello"
 	print request.values
 
@@ -89,39 +77,24 @@ def getVideoClipsNext():
 		t2 += ":00"
 
 		clips = UserQuery.get_video_clips(t1,t2)
-		''''
+
 		for clip in clips:
+			path = clip.file_path
+			clip.file_path = path.replace("avi","mp4")
 			print "The video clip id: {} start time: {} end time: {} ".format(clip.file_path,clip.start_time,clip.end_time)
-		'''
+
 
 		return render_template('displayVideoClips.html', list=True, idlist=clips)
 
 	except Exception as e:
 		logging.exception("Error while processing")
 
-@app.route('/displayImage')
-def displayImage():
-	id = request.values.get("path")
-	file_path = "/intruder_profiles/cropFrame_{}.jpg".format(id)
-	return render_template('displayImage.html',path=file_path)
-
 @app.route('/playVideo')
 def playVideo():
 	path = str(request.values.get("path"))
 	return render_template('playVid.html', path=path)
 
-@app.route('/video_clips/<file>')
-def send_video_file(file):
-	print "Inside send_video_file"
-	try:
-		file_path = "static/video_clips/{}".format(file)
-
-		with open(file_path, "rb") as bites:
-			return send_file(io.BytesIO(bites.read()), mimetype="video/mp4")
-		#return redirect("/static/video_clips/{}?adf=we".format(file))
-	except Exception as e:
-		logging.exception("Error while sending mp4 file")
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
    app.run()
